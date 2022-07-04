1b2a06ee7c0219e6ed1c67b0711a2fb8c7cafe4e
Halit Oskan <halit.oskan@and.digital>
2022-07-04 17:51:19 +0100

    working again

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { View, SafeAreaView, FlatList, Text } from 'react-native';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
 
 import { COLORS, NFTData } from '../constants';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={(itemData) => <Text>{itemData.item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
